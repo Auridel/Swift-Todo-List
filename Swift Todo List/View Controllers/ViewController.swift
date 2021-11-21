76db45206fc7659bb5182744de5e40b1dd4df49f
Oleg Efimov <0raziel0@gmail.com>
2021-11-21 18:59:23 +0300

    Initial commit

---
+++ Swift Todo List/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Todo List
+//
+//  Created by Олег Ефимов on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
